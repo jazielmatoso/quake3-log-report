1bbc9d7e75b57901477d41afec0135993c39785f
jazielmatoso <jazielsmatoso@gmail.com>
2022-06-03 01:25:00 -0300

    fix(main): add env.local file as default

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: `.env.local`,
       isGlobal: true,
     }),
     ReportModule,
